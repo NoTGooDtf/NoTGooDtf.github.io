134214ca338753f24bcfe2e32d3a501b67a3764d
NoTGooDtf <andrew.f.lear@gmail.com>
2023-09-04 19:40:30 -0400

    First commit

---
+++ pieces.js
@@ -0,0 +1,14 @@
+//white pieces
+const K = '<div class="piece" id="king">&#9812</div>'
+const Q = '<div class="piece" id="queen">&#9813</div>'
+const N = '<div class="piece" id="knight">&#9816</div>'
+const B = '<div class="piece" id="bishop">&#9815</div>'
+const R = '<div class="piece" id="rook">&#9814</div>'
+const P = '<div class="piece" id="pawn">&#9817</div>'
+//black pieces
+const k = '<div class="piece" id="king">&#9818</div>'
+const q = '<div class="piece" id="queen">&#9819</div>'
+const r = '<div class="piece" id="rook">&#9820</div>'
+const b = '<div class="piece" id="bishop">&#9821</div>'
+const n = '<div class="piece" id="knight">&#9822</div>'
+const p = '<div class="piece" id="pawn">&#9823</div>'
